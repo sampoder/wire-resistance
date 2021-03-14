12a2d2394f0443a903a49a0f84906aca71f6564f
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-03-14 13:11:15 +0000

    meta!

--- components/header.js
+++ components/header.js
@@ -1,10 +1,35 @@
 const React = require("react");
 
 export default () => (
-  <div
-    class="article-header"
-    style={{ color: "#ffffff" }}
-  >
+  <div class="article-header" style={{ color: "#ffffff" }}>
+    <title>Investigating the Resistance of Wire</title>
+    <meta name="title" content="Investigating the Resistance of Wire" />
+    <meta name="description" content="An Interactive Lab Report by Sam Poder" />
+
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Investigating the Resistance of Wire" />
+    <meta
+      property="og:description"
+      content="An Interactive Lab Report by Sam Poder"
+    />
+    <meta
+      property="og:image"
+      content="https://cloud-bdbzvtdab-hack-club-bot.vercel.app/0screenshot_2021-03-14_at_9.05.24_pm.png"
+    />
+
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta
+      property="twitter:title"
+      content="Investigating the Resistance of Wire"
+    />
+    <meta
+      property="twitter:description"
+      content="An Interactive Lab Report by Sam Poder"
+    />
+    <meta
+      property="twitter:image"
+      content="https://cloud-bdbzvtdab-hack-club-bot.vercel.app/0screenshot_2021-03-14_at_9.05.24_pm.png"
+    />
     <div style={{ maxWidth: "800px", textAlign: "center", margin: "auto" }}>
       <h2 class="dek" style={{ fontWeight: "600", marginBlockEnd: "0.2em" }}>
         Investigating the
@@ -12,7 +37,7 @@ export default () => (
       <h1 class="hed" style={{ fontWeight: "800", marginBlockEnd: "0.2em" }}>
         Resistance of <span className="glow">Wire</span>
       </h1>
-      <div class="byline" style={{ fontWeight: "400", display: 'none' }}>
+      <div class="byline" style={{ fontWeight: "400", display: "none" }}>
         An interactive exploration by Sam Poder (G10.3)
       </div>
     </div>
